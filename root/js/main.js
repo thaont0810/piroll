4fa1921d07b946cc1d9b14f069d7fdbaba6db449
ThaoNt <thaont@rikkeisfot.com>
2018-11-23 17:40:45 +0700

    to part test

---
+++ root/js/main.js
@@ -0,0 +1,21 @@
+var video = document.getElementById('video');
+var btn = document.getElementById('btn-video');
+var arr = document.getElementsByClassName('arrow');
+var pause = document.getElementsByClassName('pause');
+function playPause() {
+   if (video.paused) {
+     video.play();
+     if (arr) {
+      btn.classList.remove("arrow");
+      btn.classList.add("pause");
+
+     } 
+   } else {
+     video.pause();
+     if (pause) {
+      btn.classList.remove("pause");
+      btn.classList.add("arrow");
+
+     } 
+   }
+}
